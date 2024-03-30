1f32cc3a7ce63fab7fcdef64a87c9d3938ac8224
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-03-30 21:11:53 +0400

    Home Task Inher,Polymor,Access,Methods added

---
+++ HomeTaskInPolAcMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿using HomeTaskInPolAcMethod;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student student=new("Ali","Xudiyev",24,new int[] {1,2,3,4,5,6,8});
+        student.GetInfo(student);
+    }
+}
---
+++ HomeTaskInPolAcMethod/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskInPolAcMethod
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int[] Grades { get; set; }=new int[0];
+
+        public Student(string name, string surname, int age, int[] grades)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] % 2 == 0)
+                {
+                    Grades = Grades.Append(grades[i]).ToArray();
+                }
+            }
+            
+        }
+        public void GetInfo(Student student)
+        {
+            Console.WriteLine(student.Name + " " + student.Surname + " " + student.Age);
+            foreach (var item in student.Grades)
+            {
+                Console.Write(item + ", ");
+            }
+        }
+    }
+}
